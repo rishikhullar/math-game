e76b60116cbca7d50ce9b40530be1ca73a83d27e
Rishi Khullar <rishi.khull@gmail.com>
2013-01-22 16:23:30 -0600

    init

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var getApp = function (config) {
 	app.configure(function(){
 		app.set('views', __dirname + '/views');
 		app.set('view engine', 'ejs');
-		app.engine ('.html', require('ejs'));
-		app.engine ('.ejs', require('ejs'));
+		app.register('.html', require('ejs'));
+		app.register('.ejs', require('ejs'));
 		app.use(express.bodyParser());
 		app.use(express.methodOverride());
 		app.use(express.cookieParser());
